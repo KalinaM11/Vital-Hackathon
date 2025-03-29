350458a9ee06fbd10efb800f2908870642281246
Madison Bowman <msbowman44@gmail.com>
2025-03-29 12:06:56 -0400

    pictures!

--- vital-hacks/src/pages/homePage/homePage.components.tsx
+++ vital-hacks/src/pages/homePage/homePage.components.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
 import { Header } from '../shared/header/header.component'
 import './homePage.component.css'
 import { Button } from 'primereact/button';
 import { useNavigate } from 'react-router-dom';
+import boyImage from '../../images/boyHomePage.png';
+import girlImage from '../../images/girlHomePage.png'
 
 export const HomePageComponent = () => {
 
@@ -10,9 +11,14 @@ export const HomePageComponent = () => {
         <div>
              <Header pageType= 'Home Page'/>
              <div className='box-container'>
-                
+                <div>
+                    <img src={boyImage} alt="boyImage" style={{ height: '60vh', marginRight: '10px', padding: '10px', paddingLeft: '200px'}} />
+                    <img src={girlImage} alt="girlImage" style={{ height: '60vh', marginRight: '10px', padding: '10px'}} />
+                </div>  
              </div>
-             <Button label="start" />
+             <div className="button-container">
+                <Button label="start" className="button"/>
+             </div>
         </div>
     );
 };
